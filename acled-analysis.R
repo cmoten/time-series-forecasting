e584d0c4cc4a3dd2da270d72a9ab257ef1643e14
cmoten <cmoten@gmail.com>
2022-08-24 05:03:40 -0400

    updated data wrangling process

--- acled-analysis.R
+++ acled-analysis.R
@@ -12,30 +12,54 @@ acled_data$DATE <- format(as.Date(acled_data$EVENT_DATE, format="%d %B %Y"),"%d-
 acled_data$EVENT_MONTH <- format(as.Date(acled_data$EVENT_DATE, format="%d %B %Y"),"%B")
 
 #'Test Time Series Analysis
-test_data <- acled_data %>%
-  select(EVENT_DATE, EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY) %>%
-  mutate(Quarter = yearquarter(EVENT_DATE)) %>%
-  select(-EVENT_DATE) %>%
-  group_by(Quarter, EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY) %>%
+# test_data <- acled_data %>%
+#   select(EVENT_DATE, EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY, ACTOR1) %>%
+#   mutate(Month = yearmonth(EVENT_DATE)) %>%
+#   group_by(Month, EVENT_DATE, EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY, ACTOR1) %>%
+#   count() %>%
+#   as_tsibble(key = c(EVENT_DATE, EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY, ACTOR1),
+#              index = Month)
+# 
+# algeria_battles <- test_data %>%
+#   filter(COUNTRY == "Algeria", EVENT_TYPE == "Battles", SUB_EVENT_TYPE == "Armed clash")
+# 
+# algeria_battles %>%
+#   autoplot(n) +
+#   labs(title = "Armed Clashes 1997-2022",
+#        subtitle = "Algeria",
+#        y = "Total")
+# 
+# algeria_battles %>%
+#   fill_gaps(n = 0) %>%
+#   ACF(n) %>%
+#   autoplot() + labs(title="Algerian Clashes")
+# 
+# algeria_battles %>%
+#   gg_lag(n, geom = "point") +
+#   labs(x = "lag(Total, k)")
+
+jnim_events <- acled_data %>%
+  filter(grepl("JNIM:", ACTOR1)) %>%
+  mutate(Month = yearmonth(EVENT_DATE)) %>%
+  group_by(Month) %>%
   count() %>%
-  as_tsibble(key = c(EVENT_TYPE, SUB_EVENT_TYPE, COUNTRY),
-             index = Quarter)
+  as_tsibble(index = Month)
+
+jnim_events %>%
+  autoplot(n)
+
+jnim_events_by_type <- acled_data %>%
+  filter(grepl("JNIM:", ACTOR1)) %>%
+  mutate(Month = yearmonth(EVENT_DATE)) %>%
+  group_by(Month, EVENT_TYPE, SUB_EVENT_TYPE) %>%
+  count() %>%
+  as_tsibble(key = c(EVENT_TYPE, SUB_EVENT_TYPE),
+             index = Month)
+
+jnim_events_by_type %>%
+  autoplot(n) +
+  facet_wrap(~EVENT_TYPE + SUB_EVENT_TYPE) +
+  theme(legend.position = "None")
   
 
-algeria_battles <- test_data %>%
-  filter(COUNTRY == "Algeria", EVENT_TYPE == "Battles", SUB_EVENT_TYPE == "Armed clash")
 
-algeria_battles %>%
-  autoplot(n) +
-  labs(title = "Armed Clashes 1997-2022",
-       subtitle = "Algeria",
-       y = "Total")
-
-algeria_battles %>%
-  fill_gaps(n = 0) %>%
-  ACF(n) %>%
-  autoplot() + labs(title="Algerian Clashes")
-
-algeria_battles %>%
-  gg_lag(n, geom = "point") +
-  labs(x = "lag(Total, k)")
